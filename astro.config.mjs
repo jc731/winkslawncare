d6758b43612b46e0747e2d2870a82dbd1b67e21a
jon cagle <joncagle@unisyntechnoloiges.com>
2026-03-11 10:41:40 -0500

    Add Netlify integration and configuration
    - Update .gitignore to include local Netlify folder
    - Modify astro.config.mjs to integrate @astrojs/netlify adapter
    - Create netlify.toml for build settings and deployment configuration
    - Add netlify-cli as a dependency in package.json
    - Update pnpm-lock.yaml with new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   image: {
     service: passthroughImageService(),
   },
+
+  adapter: netlify(),
 });
